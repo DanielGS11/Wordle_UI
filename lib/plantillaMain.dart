9308cbf9c0f0aaf6d588aa481b2ac6dcb3f7c083
DanielGS11 <dgarciasalas11@gmail.com>
2025-12-04 10:46:36 +0100

    Initial commit

---
+++ lib/plantillaMain.dart
@@ -0,0 +1,41 @@
+/*
+import 'package:flutter/material.dart';
+void main() {
+  runApp(TestLayout());
+}
+
+class TestLayout extends StatelessWidget {
+  const TestLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold());
+  }
+}
+*/
+
+/*
+import 'dart:math';
+
+void main() {
+  runApp(TestLayout());
+}
+
+class TestLayout extends StatefulWidget {
+  const TestLayout({super.key});
+
+  @override
+  State<TestLayout> createState() => _TestLayoutState();
+}
+
+class _TestLayoutState extends State<TestLayout> {
+  @override
+  var img1 = "imagenes/scissors.png";
+  var img2 = "imagenes/scissors.png";
+  var winner = "Pulsa el Boton";
+
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold());
+  }
+}
+*/
